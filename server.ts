417a18e1aa439e8831e51c2c006629e86c99f516
namelesskull <khaqulhudaa@gmail.com>
2025-10-16 22:09:50 +0700

    add html

--- server.ts
+++ server.ts
@@ -1,18 +1,9 @@
 import express from 'express';
-import path from 'path';
 import { resolveCLI } from './middlewares/resolve_cli';
 import { root } from './controller/root';
 
 const app = express();
 const PORT = 8123;
-
-app.get('/globals.css', (req, res) => {
-  res.sendFile(path.join(__dirname, 'globals.css'), {
-    headers: { 'Content-Type': 'text/css' },
-  });
-});
-
-app.use(express.static(path.join(__dirname, 'html')));
 
 app.use(resolveCLI);
 app.get('/', root);
