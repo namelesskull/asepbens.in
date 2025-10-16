d8c453a97756157e9c2d388fbd5fe92d56e88b9e
namelesskull <khaqulhudaa@gmail.com>
2025-10-16 22:27:53 +0700

    add html

--- server.ts
+++ server.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import { resolveCLI } from './middlewares/resolve_cli';
 import { root } from './controller/root';
+import { resolve } from 'path';
 
 const app = express();
 const PORT = 8123;
+
+app.use(express.static(resolve(__dirname, './html')));
 
 app.use(resolveCLI);
 app.get('/', root);
